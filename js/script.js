442c023f22643fd66fdbb1eeba7280b1d945e2b3
EvoX7 <creativesoultouch@gmail.com>
2022-07-05 20:07:38 +0200

    added buttons

--- js/script.js
+++ js/script.js
@@ -31,44 +31,22 @@ const app = new Vue({
             },
         ],
 
+    },
 
-    }
+    methods: {
+        nextSlide() {
+            this.index++;
+            if (this.index >= this.slides.length) {
+                this.index = 0;
+            }
+        },
+    
+        prevSlide() {
+            this.index--;
+            if (this.index < 0) {
+                this.index = this.slides.length - 1;
+            }
+        }
+    },
+
 });
-
-
-
-
-
-
-
-
-
-// const slides = [
-//     {
-//         image: 'img/01.jpg',
-//         title: 'Svezia',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-//     },
-//     {
-//         image: 'img/02.jpg',
-//         title: 'Svizzera',
-//         text: 'Lorem ipsum.',
-//     },
-//     {
-//         image: 'img/03.jpg',
-//         title: 'Gran Bretagna',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-//     },
-//     {
-//         image: 'img/04.jpg',
-//         title: 'Germania',
-//         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
-//     },
-//     {
-//         image: 'img/05.jpg',
-//         title: 'Paradise',
-//         text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
-//     }
-// ];
-
-// console.log(slides);
